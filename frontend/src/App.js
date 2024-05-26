b83af56a584b3cf034b65516e7c983a62c744c89
Varun Singh <88921846+AlgoFoe@users.noreply.github.com>
2024-05-26 15:28:15 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Chat from './components/Chat';
 
 function App() {
-  axios.defaults.baseURL = 'https://chatapp-ayim.onrender.com'
+  axios.defaults.baseURL = 'https://chatapp-production-8f10.up.railway.app'
   axios.defaults.withCredentials = true;
   const {username,id} = useContext(UserContext);
   if(username){
